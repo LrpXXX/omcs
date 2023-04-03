9f496271fb2deaed636752596c1acd40efed0dd4
_gh <18225152643@163.com>
2023-04-03 15:31:50 +0800

    通知公告/新增/编辑

--- src/views/message/editNotific.vue
+++ src/views/message/editNotific.vue
@@ -1,83 +1,188 @@
 <template>
-    <div class="main">
-        <div style="margin-bottom: 30px;">
-            <el-input v-model="title"  placeholder="请填写公告标题"></el-input>
-        </div>
-        <div style="border: 1px solid black;margin-bottom: 20px;">
-        <Toolbar
-            style="border-bottom: 1px solid #ccc"
-            :editor="editor"
-            :defaultConfig="toolbarConfig"
-            :mode="mode"
-        />
-        <Editor
-            style="height: 500px; overflow-y: hidden;"
-            v-model="html"
-            :defaultConfig="editorConfig"
-            :mode="mode"
-            @onCreated="onCreated"
-        />
+  <div class="main">
+    <div style="margin-bottom: 30px">
+      <el-input v-model="noticeTitle" placeholder="请填写公告标题"></el-input>
     </div>
-    <div style="text-align: center;">
-        <el-button type="primary" @click="save">存为草稿</el-button>
-        <el-button @click="artilce">立即发布</el-button>
+    <div style="border: 1px solid black; margin-bottom: 20px">
+      <Toolbar style="border-bottom: 1px solid #ccc" :editor="editor" :defaultConfig="toolbarConfig" :mode="mode" />
+      <Editor style="height: 500px; overflow-y: hidden" v-model="html" :defaultConfig="editorConfig" :mode="mode" @onCreated="onCreated" />
     </div>
+    <div style="text-align: center">
+      <el-button type="primary" @click="save">存为草稿</el-button>
+      <el-button @click="artilce">立即发布</el-button>
     </div>
+  </div>
 </template>
 <script>
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import notification from "@/service/api/message/notifiction";
+import auth from "@/common/auth";
 export default {
-    name: 'MyEditor',
-    components: { Editor, Toolbar },
-    data() {
-        return {
-            title:'',
-            editor: null,
-            html: '',
-            toolbarConfig: { },
-            editorConfig: { placeholder: '请输入内容...' },
-            mode: 'simple', // or 'simple'
-        }
+  name: "MyEditor",
+  components: { Editor, Toolbar },
+  data() {
+    return {
+      noticeTitle: this.$route.query.notificRow.noticeTitle == undefined ? "" : this.$route.query.notificRow.noticeTitle,
+      notificRow: this.$route.query.notificRow == undefined ? "" : this.$route.query.notificRow,
+      editor: null,
+      html: this.$route.query.notificRow.noticeContent == undefined ? "" : this.$route.query.notificRow.noticeContent,
+      toolbarConfig: {},
+      editorConfig: { placeholder: "请输入内容..." },
+      mode: "simple", // or 'simple'
+    };
+  },
+  methods: {
+    // 创建wangEditor实例对象
+    onCreated(editor) {
+      this.editor = Object.seal(editor); // 【注意】一定要用 Object.seal() 否则会报错
+      console.log(editor.getConfig());
     },
-    methods: {
-        // 创建wangEditor实例对象
-        onCreated(editor) {
-            this.editor = Object.seal(editor) // 【注意】一定要用 Object.seal() 否则会报错
-            console.log(editor.getConfig());
-        },
-        printEditorHtml() {
-            const editor = this.editor
-            if (editor == null) return
-            console.log(editor.getHtml())
-        },
-        // 发布草稿
-        save(){
-            console.log(!this.title);
-            console.log(!this.html);
-        },
-        artilce(){
-            console.log('立即发布');
-        }
+    printEditorHtml() {
+      const editor = this.editor;
+      if (editor == null) return;
+      console.log(editor.getHtml());
     },
-    mounted() {
-        // 模拟 ajax 请求，异步渲染编辑器
-        // setTimeout(() => {
-        //     this.html = '<p>Ajax 异步设置内容 HTML</p>'
-        // }, 1500)
+    // 发布草稿
+    save() {
+      const editor = this.editor;
+      if (editor == null) return;
+      if (this.checkItemEmpty(editor)) {
+        this.$message.warning("表单不能为空!");
+        return;
+      }
+      let userInfo = JSON.parse(auth.getUserInfo().data.userInfo);
+      let data = {};
+      if (this.notificRow == undefined || this.notificRow == "") {
+        data = this.notificRow;
+        data.noticeTitle = this.noticeTitle;
+        data.noticeContent = editor.getHtml();
+        data.publishState = 0;
+      } else {
+        data = {
+          noticeTitle: this.noticeTitle,
+          noticeContent: editor.getHtml(),
+          publishPerson: userInfo.userName == undefined ? null : userInfo.userName,
+          publishState: 0,
+          displayClinet: "",
+        };
+      }
+      if (this.notificRow != undefined && this.notificRow != "") {
+        notification.updateById(JSON.stringify(data))
+        .then((res) => {
+            if (res.code == 200) {
+              this.$message.success("发布存为草稿成功！!");
+              this.$router.push("/message/notifiction");
+            } else {
+              this.$message.error("发布失败!");
+            }
+          })
+          .catch((err) => {
+            this.$message.error(err);
+          });
+      } else {
+        notification.add(JSON.stringify(data))
+          .then((result) => {
+            if (result.code == 200) {
+              this.$message.success("发布存为草稿成功！");
+
+              this.noticeTitle = "";
+              this.editor.setHtml("");
+              return;
+            }
+            this.$message.error("系统故障");
+          })
+          .catch((err) => {
+            this.$message.error(err);
+          });
+      }
     },
-    beforeDestroy() {
-        const editor = this.editor
-        if (editor == null) return
-        editor.destroy() // 组件销毁时，及时销毁 editor ，重要！！！
+    artilce() {
+      const editor = this.editor;
+      if (editor == null) return;
+      if (this.checkItemEmpty(editor)) {
+        this.$message.warning("表单不能为空!");
+        return;
+      }
+      let userInfo = JSON.parse(auth.getUserInfo().data.userInfo);
+      let data = {};
+      if (this.notificRow != undefined && this.notificRow != "") {
+        data = this.notificRow;
+        data.noticeTitle = this.noticeTitle;
+        data.noticeContent = editor.getHtml();
+        data.publishState = 1;
+        console.log(this.notificRow);
+        console.log(data);
+      } else {
+        data = {
+          noticeTitle: this.noticeTitle,
+          noticeContent: editor.getHtml(),
+          publishPerson: userInfo.userName == undefined ? null : userInfo.userName,
+          publishState: 1,
+          displayClinet: "",
+        };
+      }
+      if (this.notificRow != undefined && this.notificRow != "") {
+        debugger;
+        notification
+          .updateById(JSON.stringify(data))
+          .then((res) => {
+            if (res.code == 200) {
+              this.$message.success("发布公告成功！!");
+              this.$router.push("/message/notifiction");
+            } else {
+              this.$message.error("发布失败!");
+            }
+          })
+          .catch((err) => {
+            this.$message.error(err);
+          });
+      } else {
+        notification
+          .add(JSON.stringify(data))
+          .then((result) => {
+            if (result.code == 200) {
+              this.$message.success("发布公告成功！");
+              this.noticeTitle = "";
+              this.editor.setHtml("");
+              return;
+            }
+            this.$message.error("系统故障");
+          })
+          .catch((err) => {
+            this.$message.error(err);
+          });
+      }
     },
-}
+    checkItemEmpty(editor) {
+      console.log(editor.getHtml());
+      if (this.noticeTitle == undefined || this.noticeTitle == "") {
+        return true;
+      }
+      if (editor.getHtml() == undefined || editor.getHtml() == "" || editor.getHtml() == "<p><br></p>") {
+        return true;
+      }
+      return false;
+    },
+  },
+  mounted() {
+    // 模拟 ajax 请求，异步渲染编辑器
+    // setTimeout(() => {
+    //     this.html = '<p>Ajax 异步设置内容 HTML</p>'
+    // }, 1500)
+  },
+  beforeDestroy() {
+    const editor = this.editor;
+    if (editor == null) return;
+    editor.destroy(); // 组件销毁时，及时销毁 editor ，重要！！！
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "@wangeditor/editor/dist/css/style.css";
-.mian{
-    width: 80vw;
-    margin: 0 auto;
+.mian {
+  width: 80vw;
+  margin: 0 auto;
 }
 </style>
 
--- src/views/message/notifiction.vue
+++ src/views/message/notifiction.vue
@@ -28,62 +28,53 @@
     <commonTale :tableData="tableData" :columObj="columObj" :pageObj="pageObj" @rowOperation="rowOperation"></commonTale>
     <!-- 查看通知公告模态框 -->
     <el-dialog title="查看详情" :visible.sync="openSee">
-      <h1 style="text-align: center;">{{ seeData.title }}</h1>
-      <p style="text-indent: 2em;margin-top:10px ;">{{ seeData.main }}</p>
+      <h1 style="text-align: center">{{ seeData.noticeTitle }}</h1>
+      <p style="text-indent: 2em; margin-top: 10px">{{ seeData.noticeContent }}</p>
     </el-dialog>
   </div>
 </template>
 
 <script>
 import commonTale from "@/components/common-table/index.vue";
+import notifiction from "@/service/api/message/notifiction.js";
 export default {
   components: { commonTale },
   data() {
     return {
-      openSee:false,
+      openSee: false,
       formInline: {},
-      seeData:{},
-      tableData: [
-        {
-          id: 1,
-          title: "升级系统维护通知",
-          main: "为了向您提供稳定和更优质的服务，我们计划在2018年6月29日0点—2点",
-          time: "2015-09-27 14:30:21",
-          system: "admin",
-          ffzt: "草稿",
-        },
-        {id:2,title:'升级系统维护通知',main:'为了向您提供稳定和更优质的服务，我们计划在2018年6月29日0点—2点',time:'2015-09-27 14:30:21',system:'admin',ffzt:'已发布'}
-      ],
+      seeData: {},
+      tableData: [],
       columObj: {
         columnData: [
           {
             text: true,
-            prop: "title",
+            prop: "noticeTitle",
             label: "标题",
             align: "center",
           },
           {
             text: true,
-            prop: "main",
+            prop: "noticeContent",
             label: "内容",
             align: "center",
           },
           {
             text: true,
-            prop: "time",
+            prop: "publishTime",
             label: "时间",
             align: "center",
           },
           {
             text: true,
-            prop: "system",
-            label: "发放人",
+            prop: "publishPerson",
+            label: "发布人",
             align: "center",
           },
           {
             text: true,
-            prop: "ffzt",
-            label: "发放状态",
+            prop: "publishState",
+            label: "发布状态",
             align: "center",
           },
           {
@@ -101,18 +92,18 @@ export default {
                 // color: "red",
                 // eslint-disable-next-line no-unused-vars
                 isShow: (row, $index) => {
-                  return row.ffzt === "草稿";
+                  return row.publishState === 0;
                 },
               },
               {
-                operation: "editClose",
+                operation: "publish",
                 type: "text",
                 label: "发布",
                 icon: "",
                 // color: "blue",
                 // eslint-disable-next-line no-unused-vars
                 isShow: (row, $index) => {
-                  return row.ffzt === "草稿";
+                  return row.publishState === 0;
                 },
               },
               {
@@ -145,7 +136,7 @@ export default {
                 // color: "blue",
                 // eslint-disable-next-line no-unused-vars
                 isShow: (row, $index) => {
-                  return row.ffzt==='已发布';
+                  return row.publishState === 1;
                 },
               },
             ],
@@ -157,43 +148,161 @@ export default {
       },
     };
   },
+  created() {
+    this.initNotifiction();
+  },
   methods: {
-    // 确认取消
-    calback(){
-      this.$confirm('是否确定撤回该通知公告?', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.$message({
-            type: 'success',
-            message: '撤回成功!'
+    initNotifiction() {
+      let parmary = {
+        pageNum: 1,
+        pageSize: 20,
+      };
+      notifiction
+        .getNotificationByPage(parmary)
+        .then((res) => {
+          console.log(res);
+          if (res.code == 200) {
+            this.tableData = res.data.records;
+            this.pageObj = {
+              pageIndex: res.data.current,
+              pageSize: res.data.size,
+              total: res.data.total,
+            };
+          } else {
+            this.$message.error("系统故障!");
+          }
+        })
+        .catch((err) => {
+          this.$message.error(err);
+        });
+    },
+    /**草稿发布 */
+    publish(row) {
+      let that = this;
+      this.$confirm("是否确定发布该通知公告?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          row.publishState = 1;
+          notifiction
+            .updateById(row)
+            .then((res) => {
+              if (res.code == 200) {
+                that.$message.success("发布成功!");
+                that.initNotifiction();
+              } else {
+                that.$message.error("发布失败!");
+              }
+            })
+            .catch((err) => {
+              that.$message.error(err);
+            });
+        })
+        .catch(() => {
+          that.$message({
+            type: "info",
+            message: "已取消发布",
           });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消撤回'
-          });          
+        });
+    },
+    /**撤回公告 */
+    calback(row) {
+      let that = this;
+      this.$confirm("是否确定撤回该通知公告?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          row.publishState = 0;
+          notifiction
+            .updateById(row)
+            .then((res) => {
+              if (res.code == 200) {
+                that.$message.success("撤回成功!");
+                that.initNotifiction();
+              } else {
+                that.$message.error("撤回公告失败!");
+              }
+            })
+            .catch((err) => {
+              that.$message.error(err);
+            });
+        })
+        .catch(() => {
+          that.$message({
+            type: "info",
+            message: "已取消撤回",
+          });
+        });
+    },
+    /**删除公告 */
+    delete(row) {
+      let that = this;
+      this.$confirm("是否确定删除该通知公告?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          notifiction
+            .deleteById(row.id)
+            .then((res) => {
+              if (res.code == 200) {
+                that.$message.success("删除公告成功!");
+                that.initNotifiction();
+              } else {
+                that.$message.error("删除公告失败!");
+              }
+            })
+            .catch((err) => {
+              that.$message.error(err);
+            });
+        })
+        .catch((err) => {
+          that.$message({
+            type: "info",
+            message: "已取消删除",
+          });
         });
     },
     onSerch() {},
     onSuReg() {},
     onAdd() {
-      this.$router.push('/message/messageAdd')
+      this.$router.push("/message/messageAdd");
     },
-    rowOperation(row,$index,now){
+    onEdit(row) {
+      this.$router.push({
+                path:'/message/messageAdd',
+                query:{
+                    notificRow: row
+                }
+            })
+    },
+    rowOperation(row, $index, now) {
       switch (now) {
-        case 'see':
-          this.openSee=true;
-          this.seeData=row
+        case "see":
+          this.openSee = true;
+          this.seeData = row;
           break;
-        case 'calback':
-          this.calback()
-          break
+        case "editOpen":
+          this.onEdit(row);
+          break;
+        case "publish":
+          this.publish(row);
+          break;
+        case "calback":
+          this.calback(row);
+          break;
+        case "delete":
+          this.delete(row);
+          break;
         default:
           break;
       }
-    }
+    },
   },
 };
 </script>
